f05b64279430ba788ab66cf3570ef584316421bc
HARIMANOJ05 <harimanoj4321@gmail.com>
2024-07-13 23:25:39 +0530

    error resolved

--- setup.py
+++ setup.py
@@ -25,4 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=["numpy","pymongo","pymongo[srv]","dnspython","pandas","ensure","pytest"]
     )
