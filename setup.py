83d9836a831319d1c711bd2e6561570b646f3dcf
HARIMANOJ05 <harimanoj4321@gmail.com>
2024-07-13 22:23:13 +0530

    stage 3

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-def get_requirement(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.6"
+__version__ = "0.0.4"
 REPO_NAME = "MLOPS"
 PKG_NAME= "mongodbautomation"
 AUTHOR_USER_NAME = "HARIMANOJ05"
@@ -38,7 +25,4 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires= get_requirement("./requirements_dev.txt"),
-
-    
-)
+    )
