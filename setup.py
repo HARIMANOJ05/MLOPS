cbea1766eced09147690d7bcb92deb77a98f6ba0
HARIMANOJ05 <harimanoj4321@gmail.com>
2024-07-13 23:52:34 +0530

    version config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.2"
 REPO_NAME = "MLOPS"
 PKG_NAME= "mongodbautomation"
 AUTHOR_USER_NAME = "HARIMANOJ05"
